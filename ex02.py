a83556f3138c4267ecf7fc29195dbbde11488f96
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-09-23 22:45:12 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,6 @@
+#Esse exercício converte metros em centímetros.
+#Ex: Digite uma quantidade de metros: 5
+
+m1=int(input('Digite uma quantidade de metros:'))
+cm=m1*100
+print('A quantidade de {} metros convertida em centímetros é de {}cm.'.format(m1,cm))
