a34179f56e5396eacc846f7e99beb15dab42ca44
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-09-25 22:09:27 -0300

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,5 @@
+# Esse exercício converte reais em dólares.
+
+real=float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar=real/3.27
+print('Com R${:.2f} você pode comprar U${:.2f} .'.format(real,dolar))
