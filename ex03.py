cb296b180e61775fe5bfaa07304ed56b5138031f
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-09-25 22:07:59 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,6 @@
+# Esse exercício calcula a média entre duas notas.
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+m = (n1+n2)/2
+print('A média entre {} e {} é de {}.'.format(n1,n2,m))
