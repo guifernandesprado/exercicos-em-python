622fd52d55ffb74d761be11d726ee5e1a8724b0c
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-09-30 19:45:58 -0300

    Create ex010.py

---
+++ ex010.py
@@ -0,0 +1,5 @@
+#Esse programa mostra a porção inteira de um número real.
+
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}.'.format(num,trunc(num)))
