dbdc74110db71ca7f893447853bff547cb05ec78
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-10-06 22:35:06 -0300

    Create ex027.py

---
+++ ex027.py
@@ -0,0 +1,16 @@
+#Esse programa lê um número inteiro e mostra sua conversão para BINÁRIO, OCTAL e HEXADECIMAL.
+
+num = int(input('Digite um número inteiro: '))
+print('''Escolha uma das bases para conversão :
+[1] Converter para BINÁRIO
+[2] Converter para OCTAL
+[3] Converter para HEXADECIMAL''')
+opção = int(input('Sua opção: '))
+if opção == 1 :
+    print('{} convertido para BINÁRIO é igual a {} .'.format(num,bin(num)[2:]))
+elif opção == 2 :
+    print('{} convertido em base OCTAL é igual a {} .'.format(num,oct(num)[2:]))
+elif opção == 3 :
+    print('{} convertido em base HEXADECIMAL é igual a {} .'.format(num,hex(num)[2:]))
+else :
+    print('Opção inválida, tente novamente!')
