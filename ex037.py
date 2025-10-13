c1044ba3a7174a9b720135ad8283b51f6c518cb0
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-10-13 20:22:50 -0300

    Create ex037.py

---
+++ ex037.py
@@ -0,0 +1,9 @@
+#Esse programa faz uma contagem regressiva para o ano novo
+
+from time import sleep
+print('ATENÇÃO PARA CONTAGEM REGRESSIVA!!!!')
+for c in range (10,-1,-1):
+    sleep(1)
+    print(c)
+sleep(1)
+print('FELIZ ANO NOVO!!!')
