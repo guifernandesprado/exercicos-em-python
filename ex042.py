f5a42935524f47b37025aca459d385d745c226b4
Guilherme F. Prado <guifernandesprado@gmail.com>
2025-10-22 19:13:44 -0300

    Create ex042.py

---
+++ ex042.py
@@ -0,0 +1,8 @@
+# esse programa mostra os 10 primeiros termos de uma PA
+
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10-1) * razão
+for c in range(primeiro,décimo + razão,razão):
+    print('{}'.format(c), end=' → ')
+print('ACABOU')
